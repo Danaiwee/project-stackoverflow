be64d9d9d089422a317d501fa577a46a0526f43f
DanaiWee <Danai_wee@hotmail.com>
2025-04-11 13:39:31 +0700

    Implement Right Sidebar

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,15 @@
+import { techMap } from "@/constants/techMap";
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+};
+
+export const getDeviconClassName = (techName: string) => {
+  const normalizeTechName = techName.replace(/[ .]/g, '').toLowerCase();
+
+  return techMap[normalizeTechName]
+    ? `${techMap[normalizeTechName]} colored`
+    : "devicon-devicon-plain";
 }

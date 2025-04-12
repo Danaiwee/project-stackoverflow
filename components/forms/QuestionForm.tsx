fda5f04d172848b1464c3742883bac36b3c78854
DanaiWee <Danai_wee@hotmail.com>
2025-04-12 19:20:00 +0700

    Implement Tag Input

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -17,13 +17,15 @@ import {
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import dynamic from "next/dynamic";
+import TagCard from "../cards/TagCard";
+import { z } from "zod";
 
 const Editor = dynamic(() => import("@/components/editor"), { ssr: false });
 
 const QuestionForm = () => {
   const editorRef = useRef<MDXEditorMethods>(null);
 
-  const form = useForm({
+  const form = useForm<z.infer<typeof AskQuestionSchema>>({
     resolver: zodResolver(AskQuestionSchema),
     defaultValues: {
       title: "",
@@ -32,7 +34,51 @@ const QuestionForm = () => {
     },
   });
 
-  const handleCreateQuestion = () => {};
+  const handleInputKeyDown = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    field: { value: string[] }
+  ) => {
+    if(e.key === "Enter"){
+      e.preventDefault();
+
+      const tagInput = e.currentTarget.value.trim();
+
+      if(tagInput && tagInput.length < 15 && !field.value.includes(tagInput)){
+        form.setValue("tags", [...field.value, tagInput]);
+        e.currentTarget.value = "";
+        form.clearErrors("tags");
+
+      } else if(tagInput.length > 15){
+        form.setError("tags", {
+          type: "manual",
+          message: "Tag should be less than 15 characters"
+        });
+
+      } else if(field.value.includes(tagInput)) {
+        form.setError("tags", {
+          type: "manual",
+          message: "Tag already exists"
+        });
+      }
+    }
+  };
+
+  const handleTagRemove = (tag: string, field: {value: string[]}) => {
+    const newTags = field.value.filter((t) => t !== tag);
+
+    form.setValue("tags", newTags);
+
+    if(newTags.length === 0) {
+      form.setError("tags", {
+        type: "manual",
+        message: "Tags are required"
+      })
+    }
+  };
+
+  const handleCreateQuestion = (data: z.infer<typeof AskQuestionSchema>) => {
+    console.log(data);
+  };
 
   return (
     <Form {...form}>
@@ -74,10 +120,10 @@ const QuestionForm = () => {
                 <span className="text-primary-500">*</span>
               </FormLabel>
               <FormControl>
-                <Editor 
-                    value={field.value}
-                    editorRef={editorRef}
-                    fieldChange={field.onChange}
+                <Editor
+                  value={field.value}
+                  editorRef={editorRef}
+                  fieldChange={field.onChange}
                 />
               </FormControl>
               <FormDescription className="body-regular text-light-500">
@@ -102,9 +148,24 @@ const QuestionForm = () => {
                 <div>
                   <Input
                     className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-[56px] border"
-                    {...field}
+                    placeholder="Add tags..."
+                    onKeyDown={(e) => handleInputKeyDown(e, field)}
                   />
-                  Tags
+                  {field.value.length > 0 && (
+                    <div className="flex-start mt-2.5 flex-wrap gap-2.5">
+                      {field?.value?.map((tag: string) => (
+                        <TagCard
+                          key={tag}
+                          _id={tag}
+                          name={tag}
+                          compact
+                          remove
+                          isButton
+                          handleRemove={() => {handleTagRemove(tag, field)}}
+                        />
+                      ))}
+                    </div>
+                  )}
                 </div>
               </FormControl>
               <FormDescription className="body-regular text-light-500 mt-2">

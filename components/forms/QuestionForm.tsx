649c88bd0869d9ca64ee7bc2d97e6a1e6c000293
DanaiWee <Danai_wee@hotmail.com>
2025-04-12 17:48:08 +0700

    Implement MDX Editor

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -1,109 +1,132 @@
 "use client";
 
-import { AskQuestionSchema } from '@/lib/validations';
-import { zodResolver } from '@hookform/resolvers/zod';
-import React from 'react'
-import { useForm } from 'react-hook-form';
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '../ui/form';
-import { Input } from '../ui/input';
-import { Button } from '../ui/button';
+import { AskQuestionSchema } from "@/lib/validations";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { MDXEditorMethods } from "@mdxeditor/editor";
+import React, { useRef } from "react";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import dynamic from "next/dynamic";
+
+const Editor = dynamic(() => import("@/components/editor"), { ssr: false });
 
 const QuestionForm = () => {
+  const editorRef = useRef<MDXEditorMethods>(null);
+
   const form = useForm({
     resolver: zodResolver(AskQuestionSchema),
     defaultValues: {
-        title: "",
-        content: "",
-        tags: []
-    }
-  })
+      title: "",
+      content: "",
+      tags: [],
+    },
+  });
 
-  const handleCreateQuestion = () => {};  
+  const handleCreateQuestion = () => {};
 
   return (
     <Form {...form}>
-        <form
-            className='flex w-full flex-col gap-10'
-            onSubmit={form.handleSubmit(handleCreateQuestion)}
-        >
-            <FormField
-                control={form.control}
-                name="title"
-                render={({field}) => (
-                    <FormItem className='flex w-full flex-col'>
-                        <FormLabel className='paragraph-semibold text-dark400_light800'>
-                            Question title
-                            <span className='text-primary-500'>*</span>
-                        </FormLabel>
-                        <FormControl>
-                            <Input 
-                                className='paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-[56px] border'
-                                {...field}
-                            />
-                        </FormControl>
-                        <FormDescription className='body-regular text-light-500'>
-                            Be specific and imagine you're asking a question to another person.
-                        </FormDescription>
-                        <FormMessage />
-                    </FormItem>
-                )}
-            />
+      <form
+        className="flex w-full flex-col gap-10"
+        onSubmit={form.handleSubmit(handleCreateQuestion)}
+      >
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Question title
+                <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <Input
+                  className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-[56px] border"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription className="body-regular text-light-500">
+                Be specific and imagine you're asking a question to another
+                person.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-            <FormField 
-                control={form.control}
-                name="content"
-                render={({field}) => (
-                    <FormItem className='flex w-full flex-col'>
-                        <FormLabel className='paragraph-semibold text-dark400_light800'>
-                            Detailed explanation of your problem{" "}
-                            <span className='text-primary-500'>*</span>
-                        </FormLabel>
-                        <FormControl>Editor</FormControl>
-                        <FormDescription className='body-regular text-light-500'>
-                            Introduce the problem and expand on what you've put in the title.
-                        </FormDescription>
-                        <FormMessage />
-                    </FormItem>
-                )}
-            />
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Detailed explanation of your problem{" "}
+                <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <Editor 
+                    value={field.value}
+                    editorRef={editorRef}
+                    fieldChange={field.onChange}
+                />
+              </FormControl>
+              <FormDescription className="body-regular text-light-500">
+                Introduce the problem and expand on what you've put in the
+                title.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-            <FormField 
-                control={form.control}
-                name="tags"
-                render={({field}) => (
-                    <FormItem className='flex w-full flex-col'>
-                        <FormLabel className='paragraph-semibold text-dark400_light800'>
-                            Tags
-                            <span className='text-primary-500'>*</span>
-                        </FormLabel>
-                        <FormControl>
-                            <div>
-                                <Input 
-                                    className='paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-[56px] border'
-                                    {...field}
-                                />
-                                Tags
-                            </div>
-                        </FormControl>
-                        <FormDescription className='body-regular text-light-500 mt-2'>
-                            Add up to 3 tags to describe what you question is about. You need to press enter to add a tag.
-                        </FormDescription>
-                        <FormMessage />
-                    </FormItem>
-                )}
-            />
+        <FormField
+          control={form.control}
+          name="tags"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Tags
+                <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <div>
+                  <Input
+                    className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-[56px] border"
+                    {...field}
+                  />
+                  Tags
+                </div>
+              </FormControl>
+              <FormDescription className="body-regular text-light-500 mt-2">
+                Add up to 3 tags to describe what you question is about. You
+                need to press enter to add a tag.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-            <div className='mt-16 flex justify-end'>
-                <Button
-                    type='submit'
-                    className='primary-gradient w-fit text-light-900 cursor-pointer'
-                >
-                    Ask a Question
-                </Button>
-            </div>
-        </form>
+        <div className="mt-16 flex justify-end">
+          <Button
+            type="submit"
+            className="primary-gradient w-fit text-light-900 cursor-pointer"
+          >
+            Ask a Question
+          </Button>
+        </div>
+      </form>
     </Form>
-  )
-}
+  );
+};
 
-export default QuestionForm
+export default QuestionForm;

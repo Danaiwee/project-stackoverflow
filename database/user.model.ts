fc2f1659ba03fabbf7c6cabe536391a4c32c15bc
DanaiWee <Danai_wee@hotmail.com>
2025-04-21 14:29:43 +0700

    Implement Create & Read User Routes

--- database/user.model.ts
+++ database/user.model.ts
@@ -5,7 +5,7 @@ export interface IUser {
     username: string;
     email: string;
     bio?: string;
-    image: string;
+    image?: string;
     location?: string;
     portfolio?: string;
     reputation?: string;
@@ -18,12 +18,12 @@ const UserSchema = new Schema({
     username: {type: String, required: true},
     email: {type: String, required: true},
     bio: {type: String},
-    image: {type: String, required: true},
+    image: {type: String},
     location: {type: String},
     portfolio: {type: String},
     reputaion: {type: Number, default: 0},
 }, {timestamps: true});
 
-const User = models?.user || model<IUser>("User", UserSchema);
+const User = models?.User || model<IUser>("User", UserSchema);
 
 export default User;
